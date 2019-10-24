1b76f4a60b30b64c828ff36f2700c4638d78dabd
Alex <alexrumarthegreat@gmail.com>
2019-10-24 09:37:42 +0300

    Добавил функцию запороса на продолжение

--- Laboratories/Lab1/Program.cs
+++ Laboratories/Lab1/Program.cs
@@ -8,11 +8,27 @@ namespace Lab1
 {
     class Program
     {
+        static double Continue(string prov)
+        {
+            double a = 0;
+            do
+                Console.Write(prov);
+            while (!double.TryParse(Console.ReadLine(), out a));
+
+            return a;
+        }
+        static bool AskContinue()
+        {
+            Console.Write("\nПродолжить? (y/n): ");
+            string s = Console.ReadLine();
+            return !(s == "n");
+        }
         static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Лабораторная работа 1 - 'Решение биквадратных уравнений'");
+            Console.WriteLine("Лабораторная работа 1 - 'Решение биквадратных уравненоий'");
             Console.Title = "Выполнил:Ли М.В. Группа:ИУ5-34Б";
+
         }
     }
 }
