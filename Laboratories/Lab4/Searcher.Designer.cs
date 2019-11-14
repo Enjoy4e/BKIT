ed653cf4368a7feaf004505e8a79b1b3f0419bf8
Enjoy4u <Enjoy4u@bk.ru>
2019-11-15 00:01:08 +0300

    Lab4

---
+++ Laboratories/Lab4/Searcher.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Lab4
+{
+    partial class Searcher
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        private System.Windows.Forms.Button OpenFile;
+            
+
+
+
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Search = new System.Windows.Forms.Button();
+            this.OpenFile = new System.Windows.Forms.Button();
+            this.Timer = new System.Windows.Forms.Label();
+            this.Pattern = new System.Windows.Forms.TextBox();
+            this.WordList = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // Search
+            // 
+            this.Search.Location = new System.Drawing.Point(180, 38);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(93, 30);
+            this.Search.TabIndex = 0;
+            this.Search.Text = "Search";
+            this.Search.UseVisualStyleBackColor = true;
+            this.Search.Click += new System.EventHandler(this.Search_Click);
+            // 
+            // OpenFile
+            // 
+            this.OpenFile.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.OpenFile.Location = new System.Drawing.Point(23, 89);
+            this.OpenFile.Name = "OpenFile";
+            this.OpenFile.Size = new System.Drawing.Size(75, 27);
+            this.OpenFile.TabIndex = 1;
+            this.OpenFile.Text = "Открыть файл";
+            this.OpenFile.UseVisualStyleBackColor = true;
+            this.OpenFile.Click += new System.EventHandler(this.OpenFile_Click);
+            // 
+            // Timer
+            // 
+            this.Timer.AutoSize = true;
+            this.Timer.Location = new System.Drawing.Point(40, 21);
+            this.Timer.Name = "Timer";
+            this.Timer.Size = new System.Drawing.Size(33, 13);
+            this.Timer.TabIndex = 2;
+            this.Timer.Text = "Timer";
+            // 
+            // Pattern
+            // 
+            this.Pattern.Location = new System.Drawing.Point(154, 89);
+            this.Pattern.Multiline = true;
+            this.Pattern.Name = "Pattern";
+            this.Pattern.Size = new System.Drawing.Size(141, 27);
+            this.Pattern.TabIndex = 3;
+            // 
+            // WordList
+            // 
+            this.WordList.FormattingEnabled = true;
+            this.WordList.Location = new System.Drawing.Point(-4, 244);
+            this.WordList.Name = "WordList";
+            this.WordList.Size = new System.Drawing.Size(323, 147);
+            this.WordList.TabIndex = 4;
+            // 
+            // Searcher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 432);
+            this.Controls.Add(this.WordList);
+            this.Controls.Add(this.Pattern);
+            this.Controls.Add(this.Timer);
+            this.Controls.Add(this.OpenFile);
+            this.Controls.Add(this.Search);
+            this.Name = "Searcher";
+            this.Text = "Поиск";
+            this.Load += new System.EventHandler(this.Searcher_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Search;
+        private System.Windows.Forms.Label Timer;
+        private System.Windows.Forms.TextBox Pattern;
+        private System.Windows.Forms.ListBox WordList;
+        // private System.Windows.Forms.Button Search;
+    }
+}
---
+++ Laboratories/Lab4/Searcher.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+namespace Lab4
+{
+    public partial class Searcher : Form
+    {
+        private List<String> Words;
+        public Searcher()
+        {
+            InitializeComponent();
+        }
+
+        private void Search_Click(object sender, EventArgs e)
+        {
+            if (Words == null)
+                return;
+            String a = Pattern.Text;
+            WordList.Items.Clear();
+            WordList.BeginUpdate();
+            foreach (String x in Words)
+            {
+                if (x.ToLower().Contains(a.ToLower()))
+                {
+                    WordList.Items.Add(x);
+                }
+            }
+            WordList.EndUpdate();
+        }
+
+        private void OpenFile_Click(object sender, EventArgs e)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            OpenFileDialog readfiledialog;
+            readfiledialog = new OpenFileDialog()
+            {
+                FileName = "Select a text file",
+                Filter = "Text files (*.txt)|*.txt",
+                Title = "Open text file"
+            };
+            readfiledialog.ShowDialog();
+
+            stopwatch.Restart();
+            String[] tmp = File.ReadAllText(readfiledialog.FileName, Encoding.GetEncoding(1251)).Split(new char[] { '\n', '\r', ' ' , '.',',','!','_','?','=','-','+',':','*',';','{','}','"','[',']' });
+
+            Words = new List<String>();
+            foreach (string x in tmp)
+            {
+
+                if ((x.Trim() != "") && (!Words.Contains(x)))
+                {
+                    Words.Add(x);
+                }
+            }
+
+            stopwatch.Stop();
+            Timer.Text = "Timer  " + stopwatch.Elapsed.TotalMilliseconds + "ms";
+
+        }
+
+        private void Searcher_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+    }
+}
